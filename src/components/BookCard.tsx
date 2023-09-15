aa8057814f0acaf8ff643d15bb54c6587752dbfd
Md Imran Hossain <imranhossain1402@gmail.com>
2023-09-16 00:47:04 +0200

    fixing issue

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -12,8 +12,8 @@ export default function BookCard({ book }: IProps) {
         <div className="rounded-2xl flex flex-col items-start justify-between p-5 overflow-hidden shadow-md border border-gray-100 hover:shadow-2xl hover:scale-[102%] transition-all gap-2">
           <h1 className="text-xl font-semibold">{book?.title}</h1>
           <p>Author: {book.author} </p>
-          <p className="text-sm">Genre: ${book.genre}</p>
-          <p className="text-sm">Publication Date: ${book.publicationDate}</p>
+          <p className="text-sm">Genre: {book.genre}</p>
+          <p className="text-sm">Publication Date: {book.publicationDate}</p>
         </div>
       </Link>
     </div>
